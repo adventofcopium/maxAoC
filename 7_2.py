fd7123aa5f3ffbc1c0660e27e70a1bd880981881
Max.Gewald <max.gewald@physik.uni-muenchen.de>
2024-12-10 23:54:42 +0100

    day 7

---
+++ 7_2.py
@@ -0,0 +1,30 @@
+from itertools import product
+with open("input_7.txt") as f:
+	input =f.read().split("\n")
+results = [int(i) for i in [line.split(":")[0] for line in input]]
+#print(results)
+factors = []
+for line in input:
+	line = line.split(":")[1]
+	factors.append([int(i) for i in line[1:].split(" ")])
+#print(factors)
+c = 0
+for i, result in enumerate(results):
+	terms = factors[i]
+	perms = list(product(range(3), repeat = len(terms)-1))
+	#print(perms)
+	for perm in perms:
+		d = terms[0] 
+		for j, term in enumerate(terms[1:]):
+			#print(perms[j])
+			if perm[j] == 1:
+				d *= term
+			elif perm[j] ==2:
+				d = int(str(d) + str(term))
+			else:
+				d += term
+		#print(d)
+		if d == result:
+			c += result
+			break
+print(c)
